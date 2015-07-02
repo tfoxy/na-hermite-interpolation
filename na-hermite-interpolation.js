1566275ef8f8c636b3102d8c2372f071a2fbb281
Tomás Fox <tomas.c.fox@gmail.com>
2015-07-02 17:03:08 -0300

    Add copyright header to na-hermite-interpolation.js

--- na-hermite-interpolation.js
+++ na-hermite-interpolation.js
@@ -1,4 +1,12 @@
+/*!
+ * na-hermite-interpolation
+ * https://github.com/tfoxy/na-hermite-interpolation
+ *
+ * Copyright 2015 Tomás Fox
+ * Released under the MIT license
+ */
 module.exports = (function() {
   "use strict";
+
 
 })();
