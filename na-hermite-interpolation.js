b2463bf3c442d6725a07c5817a4ae106da8a70e4
Tomás Fox <tomas.c.fox@gmail.com>
2015-07-11 20:07:19 -0300

    Add UMD

--- na-hermite-interpolation.js
+++ na-hermite-interpolation.js
@@ -6,8 +6,21 @@
  * Released under the MIT license
  */
 
-module.exports = (function() {
-  "use strict";
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.HermiteInterpolation = factory();
+  }
+}(this, function () {
+  'use strict';
 
   var events = require('events');
 
@@ -199,4 +212,4 @@ module.exports = (function() {
 
 
   return HermiteInterpolation;
-})();
+}));
